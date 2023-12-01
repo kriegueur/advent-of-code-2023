a36c0e6fa38d09cb3e455442cb9d2bb3718b9abb
Thomas MILLER <thomas.miller@epita.fr>
2023-12-01 15:24:11 +0100

    feat: day 1 part 2

--- day-1-trebuchet/src/main.rs
+++ day-1-trebuchet/src/main.rs
@@ -1,4 +1,3 @@
-use std::fmt::format;
 use std::fs;
 use std::env;
 
@@ -15,7 +14,44 @@ fn read_lines(filename : &str) -> Vec<String> {
 }
 
 fn get_line_number(line : &str) -> u32 {
-    let mut numbers = line.chars().filter_map(|a| a.to_digit(10));
+    let mut number_vec : Vec<u32> = vec![];
+    for i in 0..line.len() {
+        if line[i..].starts_with("one") {
+            number_vec.push(1);
+        }
+        else if line[i..].starts_with("two") {
+            number_vec.push(2);
+        }
+        else if line[i..].starts_with("three") {
+            number_vec.push(3);
+        }
+        else if line[i..].starts_with("four") {
+            number_vec.push(4);
+        }
+        else if line[i..].starts_with("five") {
+            number_vec.push(5);
+        }
+        else if line[i..].starts_with("six") {
+            number_vec.push(6);
+        }
+        else if line[i..].starts_with("seven") {
+            number_vec.push(7);
+        }
+        else if line[i..].starts_with("eight") {
+            number_vec.push(8);
+        }
+        else if line[i..].starts_with("nine") {
+            number_vec.push(9);
+        }
+        else {
+            match line.chars().nth(i).unwrap().to_digit(10) {
+                Some(x) => number_vec.push(x),
+                _ => (),
+            }
+        }
+    }
+
+    let mut numbers = number_vec.iter();
     let first = numbers.next().expect(&format!("no numbers were found in line {}", line));
     let last = match numbers.last() {
         Some(number) => number,
