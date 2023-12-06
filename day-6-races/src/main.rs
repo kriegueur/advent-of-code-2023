917188f03da9d095e6f463073a26b77365dafd24
Thomas MILLER <thomas.miller@epita.fr>
2023-12-06 20:16:32 +0100

    feat: day 6 part 2

--- day-6-races/src/main.rs
+++ day-6-races/src/main.rs
@@ -9,6 +9,7 @@ fn main() {
         .map(String::from)
         .collect::<Vec<String>>();
     let re = regex::Regex::new(r"\d+").unwrap();
+    /* Part 1
     let times = re
         .find_iter(&lines[0])
         .map(|s| s.as_str().parse::<u32>().unwrap());
@@ -22,18 +23,33 @@ fn main() {
     let counts = races.map(|race| race.win_count());
     let result: u32 = counts.product();
     dbg!(result);
+    */
+    let time: u64 = re
+        .find_iter(&lines[0])
+        .fold(String::new(), |s, found_match| s + found_match.as_str())
+        .parse()
+        .unwrap();
+    let dist: String = re
+        .find_iter(&lines[1])
+        .fold(String::new(), |s, found_match| s + found_match.as_str());
+    let race = Race {
+        time,
+        distance: dist.parse().unwrap(),
+    };
+    let result = race.win_count();
+    dbg!(result);
 }
 
 #[derive(Debug)]
 struct Race {
-    time: u32,
-    distance: u32,
+    time: u64,
+    distance: u64,
 }
 
 impl Race {
-    fn win_count(&self) -> u32 {
+    fn win_count(&self) -> u64 {
         (0..=self.time)
             .filter(|press| press * (self.time - press) > self.distance)
-            .count() as u32
+            .count() as u64
     }
 }
