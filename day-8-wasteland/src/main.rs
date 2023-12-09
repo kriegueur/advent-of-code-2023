d22fc23d2782b3dcbe2d68d101ce14414159b94b
Thomas MILLER <thomas.miller@epita.fr>
2023-12-09 17:28:30 +0100

    feat: day 8 part 2 + day 9 part 1

--- day-8-wasteland/src/main.rs
+++ day-8-wasteland/src/main.rs
@@ -1,4 +1,6 @@
+use num::integer::lcm;
 use std::collections::HashMap;
+use std::collections::HashSet;
 use std::env;
 use std::fs;
 
@@ -6,9 +8,25 @@ fn main() {
     let filepath = env::args().nth(1).unwrap();
     let file_content = fs::read_to_string(filepath).unwrap();
     let mut lines = file_content.lines();
-    let documents = Documents::new(&mut lines);
-    let result: usize = documents.count();
-    dbg!(result);
+    let (instructions, documents) = parse_lines(&mut lines);
+    let starts = documents
+        .keys()
+        .filter(|k| k.chars().last().unwrap() == 'A')
+        .collect::<Vec<&String>>();
+    let mut paths: Vec<_> = starts
+        .iter()
+        .map(|s| Path::new(&instructions, &documents, s))
+        .collect();
+    let mut ranges: Vec<Range> = vec![];
+    for i in 0..paths.len() {
+        let mut path = &mut paths[i];
+        ranges.push(find_path_loop(&mut path));
+    }
+    let lcm: u64 = ranges
+        .iter()
+        .map(|r| r.get() as u64)
+        .fold(1, |a, b| lcm(a, b));
+    dbg!(lcm);
 }
 
 #[derive(Debug)]
@@ -27,51 +45,128 @@ fn instruction_list_from_line(line: &str) -> Vec<Instruction> {
         .collect()
 }
 
+fn parse_lines<'a, T: Iterator<Item = &'a str>>(
+    lines: &mut T,
+) -> (Vec<Instruction>, HashMap<String, (String, String)>) {
+    let instructions = instruction_list_from_line(lines.next().unwrap());
+    lines.next();
+    let mut documents: HashMap<String, (String, String)> = HashMap::new();
+    let re = regex::Regex::new(r"(\w{3}) = \((\w{3}), (\w{3})\)").unwrap();
+    for line in lines {
+        let captures = re.captures(line).unwrap();
+        documents.insert(
+            captures[1].to_string(),
+            (captures[2].to_string(), captures[3].to_string()),
+        );
+    }
+    (instructions, documents)
+}
+
 #[derive(Debug)]
-struct Documents {
-    instructions: Vec<Instruction>,
-    path: HashMap<String, (String, String)>,
-    current: String,
+struct Path<'a> {
+    instructions: &'a Vec<Instruction>,
+    documents: &'a HashMap<String, (String, String)>,
+    current: &'a String,
     cursor: usize,
 }
 
-impl<'a> Documents {
-    fn new<T: Iterator<Item = &'a str>>(lines: &mut T) -> Documents {
-        let instructions = instruction_list_from_line(lines.next().unwrap());
-        lines.next();
-        let mut path: HashMap<String, (String, String)> = HashMap::new();
-        let re = regex::Regex::new(r"(\w{3}) = \((\w{3}), (\w{3})\)").unwrap();
-        for line in lines {
-            let captures = re.captures(line).unwrap();
-            path.insert(
-                captures[1].to_string(),
-                (captures[2].to_string(), captures[3].to_string()),
-            );
-        }
-        Documents {
+impl<'a> Path<'a> {
+    fn new(
+        instructions: &'a Vec<Instruction>,
+        documents: &'a HashMap<String, (String, String)>,
+        start: &'a String,
+    ) -> Path<'a> {
+        Path {
             instructions,
-            path,
-            current: String::from("AAA"),
+            documents,
+            current: start,
             cursor: 0,
         }
     }
 }
 
-impl Iterator for Documents {
-    type Item = u8;
+impl<'a> Iterator for Path<'a> {
+    type Item = &'a String;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.current == "ZZZ" {
-            return None;
-        }
         let instruction = &self.instructions[self.cursor];
         self.cursor = (self.cursor + 1) % self.instructions.len();
-        let options = self.path.get(&self.current);
+        let options = self.documents.get(self.current);
         let next = match instruction {
             Instruction::Left => &options.unwrap().0,
             Instruction::Right => &options.unwrap().1,
         };
-        self.current = next.into();
-        Some(1)
+        self.current = next;
+        Some(self.current)
+    }
+}
+
+fn find_path_loop(path: &mut Path) -> Range {
+    let first = path.current;
+    let mut positions: HashSet<(usize, &String)> = HashSet::new();
+    let (start, startpos) = loop {
+        let curr: &String = path.current;
+        let pos: usize = path.cursor;
+        if positions.contains(&(pos, curr)) {
+            break (pos, curr);
+        }
+        if curr.chars().last().unwrap() == 'Z' {
+            positions.insert((pos, curr));
+        }
+        path.next();
+    };
+    let mut loopstart: usize = 0;
+    path.current = first;
+    path.cursor = 0;
+    loop {
+        if path.cursor == start && path.current == startpos {
+            break;
+        }
+        loopstart += 1;
+        path.next();
+    }
+    let mut loop_length = 1;
+    let mut jumps: Vec<usize> = vec![];
+    path.next();
+    while path.cursor != start || path.current != startpos {
+        if path.current.chars().last().unwrap() == 'Z' {
+            jumps.push(loop_length);
+            loop_length = 0;
+        } else {
+            loop_length += 1;
+        }
+        path.next();
+    }
+    jumps.push(loop_length);
+    dbg!(loopstart, &jumps);
+    Range {
+        pos: loopstart,
+        jumps,
+        cursor: 0,
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct Range {
+    pos: usize,
+    jumps: Vec<usize>,
+    cursor: usize,
+}
+
+impl Range {
+    fn get(&self) -> usize {
+        self.pos
+    }
+}
+
+impl PartialOrd for Range {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Range {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.get().cmp(&other.get())
     }
 }
---
+++ day-9-maintenance/src/main.rs
@@ -0,0 +1,25 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let filepath = env::args().nth(1).unwrap();
+    let file_content = fs::read_to_string(filepath).unwrap();
+    let histories: Vec<Vec<i32>> = file_content
+        .lines()
+        .map(|l| l.split(' ').map(|d| d.parse().unwrap()).collect())
+        .collect();
+    let result: i32 = histories.iter().map(calculate_next_value).sum();
+    dbg!(result);
+}
+
+fn calculate_next_value(history: &Vec<i32>) -> i32 {
+    if history.iter().all(|value| *value == 0) {
+        return 0;
+    };
+    let mut next: Vec<i32> = Vec::with_capacity(history.len() - 1);
+    for (prev, curr) in history.iter().zip(history.iter().skip(1)) {
+        next.push(curr - prev);
+    }
+    let after = calculate_next_value(&next);
+    history.last().unwrap() + after
+}
