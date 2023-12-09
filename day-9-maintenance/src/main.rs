14ee6a41f574af098e3a28de70df2de7aedfb04d
Thomas MILLER <thomas.miller@epita.fr>
2023-12-09 17:35:22 +0100

    feat: day 9 part 2

--- day-9-maintenance/src/main.rs
+++ day-9-maintenance/src/main.rs
@@ -8,10 +8,14 @@ fn main() {
         .lines()
         .map(|l| l.split(' ').map(|d| d.parse().unwrap()).collect())
         .collect();
+    /* part 1
     let result: i32 = histories.iter().map(calculate_next_value).sum();
+    */
+    let result: i32 = histories.iter().map(calculate_prev_value).sum();
     dbg!(result);
 }
 
+/* part 1
 fn calculate_next_value(history: &Vec<i32>) -> i32 {
     if history.iter().all(|value| *value == 0) {
         return 0;
@@ -22,4 +26,17 @@ fn calculate_next_value(history: &Vec<i32>) -> i32 {
     }
     let after = calculate_next_value(&next);
     history.last().unwrap() + after
+}
+*/
+
+fn calculate_prev_value(history: &Vec<i32>) -> i32 {
+    if history.iter().all(|value| *value == 0) {
+        return 0;
+    };
+    let mut next: Vec<i32> = Vec::with_capacity(history.len() - 1);
+    for (prev, curr) in history.iter().zip(history.iter().skip(1)) {
+        next.push(curr - prev);
+    }
+    let before = calculate_prev_value(&next);
+    history.first().unwrap() - before
 }
